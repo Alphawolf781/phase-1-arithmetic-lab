b95ecb0e200ffbaef929ee2723a169805ac4c7e3
Alphawolf781 <vincent.wambua@student.moringaschool.com>
2024-07-11 18:55:00 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+ const multiply = num1*num2
+ console.log(multiply)
+
+ let random = Math.floor(Math.random()* Number.MAX_SAFE_INTEGER)+1
+ console.log(random)
+
+ let num3 = 14
+ let num4 = 10
+const mod = num3 % num4
+console.log(mod)
+
+let set = [5, 12, 15, 20, 19]
+const max = Math.max(...set)
